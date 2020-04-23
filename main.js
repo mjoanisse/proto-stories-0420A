c0a25608484d7e64c57bf943de3c54e3fdc4c168
Michel Joanisse <mich.joanisse@gmail.com>
2020-04-23 14:30:50 -0400

    Rough draft build update.

--- main.js
+++ main.js
@@ -1,19 +1,52 @@
+var protoNavLink;
+// ***
+function setSelectedLinkClass() {
+  var onSection = $('.chh-narrative.on');
+  var onSectionId = onSection.attr('data-id');
+  protoNavLink.removeClass('on');
+  var matchingLink = protoNavLink.filter('[data-get-id="'+onSectionId+'"]');
+  matchingLink.addClass('on');
+}
+function minimizePreviewBox() {
+  var previewBox = $('.preview-box');
+  previewBox.addClass('rev-trans').removeClass('full');
+  setTimeout(() => {
+    previewBox.removeClass('rev-trans');
+  }, 1);
+}
+function enableMainScroll() {
+  $('html, body').removeClass('scroll-none');
+}
+function disableMainScroll() {
+  $('html, body').addClass('scroll-none');
+}
+$(document).on('keydown', function(e) {
+  if (e.key == 'Escape') {
+    minimizePreviewBox();
+    enableMainScroll();
+  }
+});
 $(function() {
-  var protoNavLink = $('.proto-nav a');
+  protoNavLink = $('.proto-nav a');
   var previewBoxButton = $('.pb-show-all-button');
+  setSelectedLinkClass();
   // ***
   protoNavLink.click(function(e) {
     e.preventDefault();
     var dataId = $(this).attr('data-get-id');
     $('.chh-narrative').removeClass('on');
     var selSection = $('[data-id="'+dataId+'"]');
+    // console.log(selSection);
     $('.preview-box').removeClass('on');
     selSection.addClass('on');
     selSection.find('.preview-box').addClass('on');
+    setSelectedLinkClass();
   });
   previewBoxButton.mousedown(function() {
     $('.preview-box__main').addClass('on');
   }).mouseup(function() {
+    disableMainScroll();
+    $('.preview-box').addClass('full');
     $('.preview-box__main').removeClass('on');
   });
 });
